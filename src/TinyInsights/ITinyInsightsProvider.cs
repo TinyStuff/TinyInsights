27ebbc3491aa5e7237c5383eba1f4cec852bfe07
Daniel Hindrikes <daniel@hindrikes.se>
2018-02-26 15:19:49 +0100

    tiny instights core architecture

--- src/TinyInsights/ITinyInsightsProvider.cs
+++ src/TinyInsights/ITinyInsightsProvider.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TinyInsights
+namespace TinyInsightsLib
 {
-    public interface ITinyInsights
+    public interface ITinyInsightsProvider
     {
+        bool IsTrackErrorsEnabled { get; set; }
+        bool IsTrackPageViewsEnabled  { get; set; }
+        bool IsTrackEventsEnabled  { get; set; }
+
         Task TrackErrorAsync(Exception ex);
 
-        Task TrackPageViewAsync(string viewname);
+        Task TrackPageViewAsync(string viewName);
         Task TrackPageViewAsync(string viewName, Dictionary<string, string> properties);
 
         Task TrackEventAsync(string eventName);
--- src/TinyInsights/TinyInsights.cs
+++ src/TinyInsights/TinyInsights.cs
@@ -1,9 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace TinyInsights
+namespace TinyInsightsLib
 {
     public static class TinyInsights
     {
-        public static ITinyInsights Current { get; set; }
+        private static List<ITinyInsightsProvider> insightsProviders = new List<ITinyInsightsProvider>();
+
+        public static void Configure(ITinyInsightsProvider provider)
+        {
+            insightsProviders.Add(provider);
+        }
+
+        public static void Configure(params ITinyInsightsProvider[] providers)
+        {
+            insightsProviders.AddRange(providers.ToList());
+        }
+
+        public static async Task TrackErrorAsync(Exception ex)
+        {
+            var tasks = new List<Task>();
+            
+            foreach(var provider in insightsProviders)
+            {
+                if(provider.IsTrackErrorsEnabled)
+                {
+                    var task = provider.TrackErrorAsync(ex);
+                    tasks.Add(task);
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public static async Task TrackPageViewAsync(string viewName)
+        {
+            var tasks = new List<Task>();
+
+            foreach (var provider in insightsProviders)
+            {
+                if (provider.IsTrackPageViewsEnabled)
+                {
+                    var task = provider.TrackPageViewAsync(viewName);
+                    tasks.Add(task);
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public static async Task TrackPageViewAsync(string viewName, Dictionary<string, string> properties)
+        {
+            var tasks = new List<Task>();
+
+            foreach (var provider in insightsProviders)
+            {
+                if (provider.IsTrackPageViewsEnabled)
+                {
+                    var task = provider.TrackPageViewAsync(viewName, properties);
+                    tasks.Add(task);
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public static async Task TrackEventAsync(string eventName)
+        {
+            var tasks = new List<Task>();
+
+            foreach (var provider in insightsProviders)
+            {
+                if (provider.IsTrackEventsEnabled)
+                {
+                    var task = provider.TrackEventAsync(eventName);
+                    tasks.Add(task);
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public static async Task TrackEventAsync(string eventName, Dictionary<string, string> properties)
+        {
+            var tasks = new List<Task>();
+
+            foreach (var provider in insightsProviders)
+            {
+                if (provider.IsTrackEventsEnabled)
+                {
+                    var task = provider.TrackEventAsync(eventName, properties);
+                    tasks.Add(task);
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
     }
 }
