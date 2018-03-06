dc29e4aa8b7bc36450b5db74b49620aef3d8284f
Daniel Hindrikes <daniel@hindrikes.se>
2018-03-06 15:25:36 +0100

    created google analytics project

---
+++ src/TinyInsights.GoogleAnalytics/iOS/GoogleAnalyticsProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TinyInsights;
+using TinyInsightsLib;
+
+namespace TinyInsights.GoogleAnalytics.iOS
+{
+    public class GoogleAnalyticsProvider : ITinyInsightsProvider
+    {
+        public bool IsTrackErrorsEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsTrackPageViewsEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsTrackEventsEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task TrackErrorAsync(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackEventAsync(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackEventAsync(string eventName, Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackPageViewAsync(string viewName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackPageViewAsync(string viewName, Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
